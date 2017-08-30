8f08a06378935598d591d3f9f5a425c79689b1ae
KUDO-PC\kudo <whisky4290@gmail.com>
2017-08-30 22:32:22 +0900

    【課題】数値オブジェクト・変数の宣言と表示（四則演算）　提出します

---
+++ CampChallenge_Ruby/CampChallenge_Ruby/003.数値/kadai_sisokuennzann.rb
@@ -0,0 +1,17 @@
+﻿NUMBER = 3.14
+num = 50
+
+
+a = NUMBER + num
+b = NUMBER - num
+c = NUMBER * num
+d = NUMBER / num
+
+
+print(a ,"\n")
+
+print(b ,"\n")
+
+print(c ,"\n")
+
+print(d.round(5) ,"\n")
