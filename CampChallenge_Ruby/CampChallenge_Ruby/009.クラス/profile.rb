cfb3da564d121053cea5993f81a1461cb50995f2
KUDO-PC\kudo <whisky4290@gmail.com>
2017-09-01 14:09:05 +0900

    【課題】メソッド・メソッドの作成　提出します

---
+++ CampChallenge_Ruby/CampChallenge_Ruby/009.クラス/profile.rb
@@ -0,0 +1,5 @@
+require './profile_class.rb'
+profile = Profile.new
+10.times do
+profile.me
+end
---
+++ CampChallenge_Ruby/CampChallenge_Ruby/009.クラス/profile_class.rb
@@ -0,0 +1,7 @@
+class Profile
+  def me
+    print("工藤" ,"\n")
+    print("1993/04/29" ,"\n")
+    print("マレーシアに住んでいました" ,"\n")
+  end
+end
