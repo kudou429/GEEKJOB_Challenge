e5ea450b64606bca11da46c83c6bd18f67b28368
kudou shohei <whisky4290@gmail.com>
2017-09-05 16:16:22 +0900

    【課題】メソッド、戻り値２　提出します

---
+++ CampChallenge_Ruby/CampChallenge_Ruby/009.クラス/modori2.rb
@@ -0,0 +1,6 @@
+require './modori2_class.rb'
+modori2 = Modori2.new
+array = modori2.data
+print(array[1])
+print(array[2])
+print(array[3])
---
+++ CampChallenge_Ruby/CampChallenge_Ruby/009.クラス/modori2_class.rb
@@ -0,0 +1,5 @@
+class Modori2
+  def data(array=["NO.4", "工藤", "1993/04/29", "東京都"])
+    return array
+  end
+end
