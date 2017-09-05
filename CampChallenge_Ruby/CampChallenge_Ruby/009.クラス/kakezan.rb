2a548820a0bccc9252054b05bb3e241b46dd82e1
kudou shohei <whisky4290@gmail.com>
2017-09-05 16:11:59 +0900

    【課題】メソッド、引数２　提出します。

---
+++ CampChallenge_Ruby/CampChallenge_Ruby/009.クラス/kakezan.rb
@@ -0,0 +1,4 @@
+require './kakezan_class.rb'
+
+kakezan = Kakezan.new
+kakezan.hikisu(5)
---
+++ CampChallenge_Ruby/CampChallenge_Ruby/009.クラス/kakezan_class.rb
@@ -0,0 +1,9 @@
+class Kakezan
+  def hikisu(num1, num2=5,  seigo="false")
+  if seigo == "ture"
+    print(num1 * num2)**2
+  elsif seigo == "false"
+    print(num1 * num2)
+  end
+  end
+end
