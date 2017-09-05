ed5674af953fa5150e7437d6bcbe41029980c470
kudou shohei <whisky4290@gmail.com>
2017-09-05 16:17:59 +0900

    【課題】メソッド、引数戻り値１　提出します

---
+++ CampChallenge_Ruby/CampChallenge_Ruby/009.クラス/innsuumodorichi1.rb
@@ -0,0 +1,4 @@
+require './innsuumodorichi1_class.rb'
+innsuumodorichi1 = Innsuumodorichi1.new
+array = innsuumodorichi1.data
+print(array[0])
---
+++ CampChallenge_Ruby/CampChallenge_Ruby/009.クラス/innsuumodorichi1_class.rb
@@ -0,0 +1,8 @@
+class Innsuumodorichi1
+  def data(array=["a", "b", "c"])
+    array[0] = [0, "一郎", "1991/01/01", "北海道"]
+    array[1] = [1, "二郎", "1992/02/02", "沖縄"]
+    array[2] = [2, "三郎", "1993/03/03", "東京"]
+  return array
+  end
+end
