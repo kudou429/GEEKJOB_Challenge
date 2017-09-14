3e719d87d6f461d9f23720181da6def610e268b2
kudou shohei <whisky4290@gmail.com>
2017-09-14 21:36:09 +0900

    課題　クエリストリング（簡易素因数分解）　提出します。

---
+++ CampChallenge_Ruby/CampChallenge_Ruby/012.ブラウザでのRuby/soinnsuu/controllers/form_request_controller.rb
@@ -0,0 +1,16 @@
+class FormRequestController
+  def call(env)
+
+
+
+request = Rack::Request.new(env)
+    @params = request.params()
+
+    Rack::Response.new(render("form_request.html.erb"))
+  end
+
+  def render(template)
+    path = File.expand_path("../../views/#{template}", __FILE__)
+    ERB.new(File.read(path)).result(binding)
+  end
+end
