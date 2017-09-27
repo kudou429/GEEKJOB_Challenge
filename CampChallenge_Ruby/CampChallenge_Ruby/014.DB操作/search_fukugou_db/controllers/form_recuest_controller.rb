8cfc7b043535457fcb04867b7bd240d6c0d97819
kudou shohei <whisky4290@gmail.com>
2017-09-27 21:12:46 +0900

    課題　複合検索　提出します。

---
+++ CampChallenge_Ruby/CampChallenge_Ruby/014.DB操作/search_fukugou_db/controllers/form_recuest_controller.rb
@@ -0,0 +1,17 @@
+class FormRecuestController
+  def call(env)
+
+
+
+request = Rack::Request.new(env)
+    @params = request.params()
+
+
+    Rack::Response.new(render("form_recuest.html.erb"))
+  end
+
+  def render(template)
+    path = File.expand_path("../../views/#{template}", __FILE__)
+    ERB.new(File.read(path)).result(binding)
+  end
+end
